147137c390054a1463d096419ac28883ca87d4c4
Robert Winterstein <rrwinterstein@gmail.com>
2021-07-20 21:17:16 -0400

    updates lodash exercise

--- projects/javascript/lodash/_.js
+++ projects/javascript/lodash/_.js
@@ -51,7 +51,51 @@ const _ = {
     if (key in object) return true;
     return false;
   },
+
+  invert(object) {
+    const invertedObj = {};
+    for (const key in object) {
+      let value = object[key];
+      let newKey = value;
+      let newValue = key;
+      invertedObj[newKey] = newValue;
+    }
+    return invertedObj;
+  },
+
+  findKey(object, predicate) {
+    for (const key in object) {
+      const value = object[key];
+      const predReturnVal = predicate(value);
+      if (predReturnVal) {
+        return key;
+      }
+    }
+    return undefined;
+  },
+
+  drop(array, num) {
+    if (num === undefined) num = 1;
+    return array.slice(num);
+  },
+
+  chunk(array, size) {
+    if (!size) size = 1;
+
+    const newArr = [];
+
+    for (let i = 0; i <= size; i++) {
+      if (!array[0]) break;
+
+      let start = 0;
+      let end = size;
+
+      newArr.push(array.slice(start, end))
+      array.splice(start, size)
+    }
+
+    return newArr;
+  },
 };
 
-// Do not write or modify code below this line.
 module.exports = _;
